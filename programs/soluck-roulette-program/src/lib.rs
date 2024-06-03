ac0be3f2d63ee230d060d127ed513040cc674d50
merdoyovski <merdoyovski@gmail.com>
2024-06-03 17:14:46 +0300

    build: added init_config method to the program

--- programs/soluck-roulette-program/src/lib.rs
+++ programs/soluck-roulette-program/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
+use std::mem::size_of;
 
 declare_id!("EesrPZXx8b6hwLcAMBFtA7RZHyuvs7oFshBoDiK23VBk");
 
@@ -6,10 +8,69 @@ declare_id!("EesrPZXx8b6hwLcAMBFtA7RZHyuvs7oFshBoDiK23VBk");
 pub mod soluck_roulette_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn init_config(ctx: Context<InitConfig>) -> Result<()> {
+      let config = &mut ctx.accounts.config;
+      
+        if config.is_init == true  {
+            return Err(RouletteErrors::ConfigAlreadyInitialized.into());
+        }
+
+        config.is_init = true;
+        config.auth_1 = *ctx.accounts.auth_1.key;
+        config.auth_2 = *ctx.accounts.auth_2.key;
+        config.auth_3 = *ctx.accounts.auth_3.key;
+        config.auth_4 = *ctx.accounts.auth_4.key;
+        config.auth_5 = *ctx.accounts.auth_5.key;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitConfig<'info> {
+    #[account(
+        init,
+        seeds = [b"config"],
+        bump,
+        payer = auth_1,
+        space = size_of::<ConfigAccounts>()*2,
+    )]
+    pub config: Account<'info, ConfigAccounts>,
+
+    #[account(mut)]
+    pub auth_1: Signer<'info>,
+    #[account(mut)]
+    /// CHECK
+    pub auth_2: AccountInfo<'info>,
+    #[account(mut)]
+      /// CHECK
+    pub auth_3: AccountInfo<'info>,
+    #[account(mut)]
+      /// CHECK
+    pub auth_4: AccountInfo<'info>,
+    #[account(mut)]
+      /// CHECK
+    pub auth_5: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct ConfigAccounts {
+    pub is_init: bool,
+    pub auth_1: Pubkey,
+    pub auth_2: Pubkey,
+    pub auth_3: Pubkey,   
+    pub auth_4: Pubkey,
+    pub auth_5: Pubkey,
+}
+
+
+#[error_code]
+pub enum RouletteErrors {
+    #[msg("Config already initialized")]
+    ConfigAlreadyInitialized,
+    #[msg("Not an authority")]
+    NotAuth,
+}
--- tests/soluck-roulette-program.ts
+++ tests/soluck-roulette-program.ts
@@ -1,16 +1,65 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SoluckRouletteProgram } from "../target/types/soluck_roulette_program";
+import { PublicKey } from "@solana/web3.js";
+
+const LAMPORTS_PER_SOL = 1000000000;
 
 describe("soluck-roulette-program", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SoluckRouletteProgram as Program<SoluckRouletteProgram>;
+  const program = anchor.workspace
+    .SoluckRouletteProgram as Program<SoluckRouletteProgram>;
+  const connection = program.provider.connection;
+  const auth_1 = anchor.web3.Keypair.generate();
+  const auth_2 = anchor.web3.Keypair.generate();
+  const auth_3 = anchor.web3.Keypair.generate();
+  const auth_4 = anchor.web3.Keypair.generate();
+  const auth_5 = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const [configPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("config")],
+    program.programId
+  );
+
+  before(async function () {
+    try {
+      const requestAirdrop = await program.provider.connection.requestAirdrop(
+        auth_1.publicKey,
+        LAMPORTS_PER_SOL * 100 // 100 SOL
+      );
+      const latestBlockHash = await connection.getLatestBlockhash();
+      await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: requestAirdrop,
+      });
+      try {
+        await program.methods
+          .initConfig()
+          .accounts({
+            config: configPDA,
+            auth1: auth_1.publicKey,
+            auth2: auth_2.publicKey,
+            auth3: auth_3.publicKey,
+            auth4: auth_4.publicKey,
+            auth5: auth_5.publicKey,
+          })
+          .signers([auth_1])
+          .rpc();
+
+        let configState = await program.account.configAccounts.fetch(configPDA);
+
+        console.log("configState: ", configState);
+        console.log("configPDA: ", configPDA.toBase58());
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   });
+
+  it("Is initialized!", async () => {});
 });
